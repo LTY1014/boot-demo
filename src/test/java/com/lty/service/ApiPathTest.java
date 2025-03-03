177cd46fa7ed1f1767ae5082a2772c6aef24522e
lty <liangtianyu011014@163.com>
2025-03-03 20:15:54 +0800

    feat: 添加OpenAPI解析

---
+++ src/test/java/com/lty/service/ApiPathTest.java
@@ -0,0 +1,40 @@
+package com.lty.service;
+
+import com.lty.util.ServletUtil;
+import org.openapi4j.parser.OpenApi3Parser;
+import org.openapi4j.parser.model.v3.OpenApi3;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URL;
+
+public class ApiPathTest {
+    public static void main(String[] args) {
+        try {
+            HttpServletRequest request = ServletUtil.getRequest();
+            String apiUrl = "http://localhost:8088/api/v3/api-docs";
+            //String baseUrl = request.getContextPath();
+            String baseUrl = "/api";
+
+            // 解析远程 OpenAPI 文档
+            OpenApi3 api = new OpenApi3Parser().parse(new URL(apiUrl), false);
+
+            // 逐层提取路由信息
+            api.getPaths().forEach((path, pathItem) -> {
+                System.out.println("接口路径: " + path);
+                // 去除前缀/api
+                path = path.replace(baseUrl, "");
+                System.out.println(" |-- 路径: " + path);
+                
+                // 获取路径下的操作方法（Operation）
+                pathItem.getOperations().forEach((httpMethod, operation) -> {
+                    System.out.println(" |-- 接口方式: " + httpMethod);
+                    System.out.println(" |-- 接口名: " + operation.getOperationId());
+                    System.out.println(" |-- 接口描述: " + operation.getSummary());
+                });
+            });
+            
+        } catch (Exception e) {
+            System.err.println("解析错误：" + e.getMessage());
+        }
+    }
+}
